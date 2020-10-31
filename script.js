50506101c9af82ada4cd490ffe433184ba4828bd
Lauren Harmon <harmon.lauren@hotmail.com>
2020-10-30 21:49:00 -0500

    revisions

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 window.addEventListener("load", function() {
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         console.log(json);
+         let planetContainer = document.getElementById("missionTarget");
+         planetContainer.innerHTML += `
+         <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[1].name}</li>
+               <li>Diameter: ${json[1].diameter}</li>
+               <li>Star: ${json[1].star}</li>
+               <li>Distance from Earth: ${json[1].distance}</li>
+               <li>Number of Moons: ${json[1].moons}</li>
+            </ol>
+         <img src="${json[1].image}"></img>
+         `;
+      });
+   });
+
+
    let form = document.querySelector("form");
    form.addEventListener("submit", function() {
       let pilot = document.querySelector("input[name=pilotName]");
@@ -13,17 +33,17 @@ window.addEventListener("load", function() {
 //Section  1 Requirements
       if (pilot.value === "" || coPilot.value === "" || fuelLevel.value === "" || mass.value === "") {
          window.alert("All fields required!");
-         event.preventDefault();
+         submit.preventDefault();
       } 
 
       if (!isNaN(pilot.value) || (!isNaN(coPilot.value))) {
          window.alert("Pilot and Co-Pilot names must be Strings!");
-         event.preventDefault();
+         submit.preventDefault();
       } 
       
       if (isNaN(fuelLevel.value) || isNaN(mass.value)) {
          window.alert("Fuel Level and Cargo Mass must be Numbers");
-         event.preventDefault();
+         submit.preventDefault();
       }
 
 // Section 2 Requirements
@@ -32,40 +52,31 @@ window.addEventListener("load", function() {
          document.getElementById("copilotStatus").innerHTML = `Co-pilot ${coPilot.value} is ready for launch.`;
       }
 
-
       if (fuelLevel.value < 10000) {
          faultyList.style.visibility = "visible";
          fuelNotReady.innerHTML = `There is not enough fuel for the journey.`;
          launchChange.innerHTML = "Shuttle not ready for launch.";
          launchChange.style.color = "red";  
-      } else if (mass.value > 10000) {
+      } else {
+         fuelNotReady.innerHTML = "There is enough fuel for the journey.";
+      }
+      
+      if (mass.value > 10000) {
          faultyList.style.visibility = "visible";
          cargoNotReady.innerHTML = `There is too much cargo to launch.`;
          launchChange.innerHTML = `Shuttle not ready for launch.`;
          launchChange.style.color = "red";
       } else {
+         cargoNotReady.innerHTML = "Cargo prepared to launch.";
+      }
+      
+      if (fuelLevel.value > 10000 && mass.value < 10000) {
          launchChange.innerHTML = `Shuttle is Ready for Launch!`;
          launchChange.style.color = "green";
-      }
+         faultyList.style.visibility = "hidden";
+      };
 
-// Section 3 Requirements
-      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
-         response.json().then(function(json) {
-            console.log(json);
-            let planetContainer = document.getElementById("missionTarget");
-            planetContainer.innerHTML += `
-            <h2>Mission Destination</h2>
-               <ol>
-                  <li>Name: ${json[1].name}</li>
-                  <li>Diameter: ${json[1].diameter}</li>
-                  <li>Star: ${json[1].star}</li>
-                  <li>Distance from Earth: ${json[1].distance}</li>
-                  <li>Number of Moons: ${json[1].moons}</li>
-               </ol>
-            <img src="${json[1].image}"></img>
-            `;
-         });
-      });
+
    });
 
 });
